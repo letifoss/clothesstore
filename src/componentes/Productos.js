db93e7394c8237f9d70935989ccadb79cf3dfbff
letifoss <letifossemale@gmail.com>
2024-06-23 14:19:48 -0300

    ajustes carrito y botones

--- src/componentes/Productos.js
+++ src/componentes/Productos.js
@@ -5,9 +5,16 @@ function Productos({ producto, addToCart }) {
     const { id, imagen, nombre, precio } = producto;
     const [agregado, setAgregado] = useState(false);
 
+
+    const [alerta, setAlerta] = useState(false);
+        
     const handleAddToCart = () => {
         addToCart(producto);
         setAgregado(true); 
+        setAlerta(true);
+        setTimeout(() => {
+            setAlerta(false);
+        }, 1500);
     };
 
     return (
@@ -16,10 +23,15 @@ function Productos({ producto, addToCart }) {
                 <img src={imagen} alt={nombre} width={150} height={180} className="mt-3" />
                 <h6>{nombre}</h6>
                 <p className="border border-dark rounded p-1">{precio}</p>
-                <button className="p-1 mb-3" onClick={handleAddToCart} disabled={agregado}>
-                    {agregado ? 'Agregado al carrito ✔️' : 'Agregar al carrito 🛒'}
+                <button className="p-1 mb-3" onClick={handleAddToCart}>
+                    Agregar al carrito 🛒
                 </button>
             </div>
+            {alerta && (
+                <div className="alert" role="alert">
+                    ¡Agregado al carrito!
+                </div>
+            )}
         </div>
     );
 }
